8871e1d2a581c0c0165a843834d69e9fbb2b7cb7
DanFariasM <danfariasm@gmail.com>
2023-03-06 22:34:57 -0500

    Blog showing the title, date and post text.

--- chapter_19_exercises/ex_19_1_blog/blogs/views.py
+++ chapter_19_exercises/ex_19_1_blog/blogs/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+from .models import BlogPost
+
 def index(request):
-    """The main page for the Blog."""
-    return render(request, "blogs/index.html")
+    """The main page for the Blog. Includes posts and dates."""
+    posts = BlogPost.objects.all()
+    context = {"posts": posts}
+    return render(request, "blogs/index.html", context)
