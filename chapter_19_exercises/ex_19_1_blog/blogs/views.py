f893e0cb0055ad6d2adb97940710359bc8fa98ec
DanFariasM <danfariasm@gmail.com>
2023-04-03 22:51:09 -0400

    End of Chapter 19.

--- chapter_19_exercises/ex_19_1_blog/blogs/views.py
+++ chapter_19_exercises/ex_19_1_blog/blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 from .models import BlogPost
 from .forms import BlogPostForm
@@ -9,6 +11,7 @@ def index(request):
     context = {"posts": posts}
     return render(request, "blogs/index.html", context)
 
+@login_required
 def new_post(request):
     """Add a new post."""
     if request.method != "POST":
@@ -18,16 +21,22 @@ def new_post(request):
         # POST data is submitted. Process data.
         form = BlogPostForm(data=request.POST)
         if form.is_valid():
-            form.save()
+            new_post = form.save(commit=False)
+            new_post.owner = request.user
+            new_post.save()
             return redirect("blogs:index")
 
     # Display a blank or invalid form.
     context = {"form": form}
     return render(request, "blogs/new_post.html", context)
 
+@login_required
 def edit_post(request, post_id):
     """Edit an existing post."""
     post = BlogPost.objects.get(id=post_id)
+
+    if post.owner != request.user:
+        raise Http404
 
     if request.method != "POST":
         # Initial request; pre-fill the form with the current post.
